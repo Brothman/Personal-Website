4ca01ed6b732d5ffdd92f528e6f0fa4c5ac2d5b6
Benji Rothman <brothman7000@gmail.com>
2018-10-08 11:12:49 -0400

    Styled the learn-more button on the about page to have a box-shadow and transition on opacity and box-shadow

--- bundle.js
+++ bundle.js
@@ -1329,20 +1329,20 @@ function (_React$Component) {
     _classCallCheck(this, WritingsPage);
 
     _this = _possibleConstructorReturn(this, _getPrototypeOf(WritingsPage).call(this, props));
+    _this.state = {
+      writingIndexItemArr: []
+    };
     _this.viewWriting = _this.viewWriting.bind(_assertThisInitialized(_assertThisInitialized(_this)));
+    _this.filterByDate = _this.filterByDate.bind(_assertThisInitialized(_assertThisInitialized(_this)));
+    _this.filterByTimeToRead = _this.filterByTimeToRead.bind(_assertThisInitialized(_assertThisInitialized(_this)));
     return _this;
   }
 
   _createClass(WritingsPage, [{
-    key: "viewWriting",
-    value: function viewWriting(id) {
-      this.props.history.push("/writings/".concat(id));
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this2 = this;
-
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      //date.getTime() gives the unix timecode stamp
+      //0 for month represents january
       var writing24 = {
         id: 24,
         title: "Lost Confidence",
@@ -1350,6 +1350,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "May 14, 2018",
         readTime: "15 min read",
+        intTime: 15,
+        intDate: new Date(2018, 4, 14),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Mr-Nobody.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQoQCGzB2GhtU4KW3vtpKuv53IdYbCFFesmfsPm_OXnyenJSMU-SiO6n0duuQptLfdcHJA84M8q914h/pub?embedded=true"
       };
@@ -1360,6 +1362,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Mar 16, 2018",
         readTime: "10 min read",
+        intTime: 10,
+        intDate: new Date(2018, 2, 16),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Know-Thyself.jpeg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQPfJ5yDEOfhguipnBsSFl-45NjaS5jLc6jc5R8-ZiOk9i0d9GGeQH8FS6vkEKAP_lT4taccdsD3IG_/pub?embedded=true"
       };
@@ -1370,6 +1374,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Mar 2, 2018",
         readTime: "10 min read",
+        intTime: 10,
+        intDate: new Date(2018, 2, 2),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/yale-protest.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vSYFJQg7zwlptr_wUzOkn2GVcWwgDChFyZ069hlzXVBqCt83yVsmI923Ujy-em-izw8EJOGyJm_qZsA/pub?embedded=true"
       };
@@ -1380,6 +1386,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Dec 13, 2017",
         readTime: "30 min read",
+        intTime: 30,
+        intDate: new Date(2017, 11, 13),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Human_Nature.jpg"
       };
       var writing20 = {
@@ -1389,6 +1397,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Oct 18, 2017",
         readTime: "5 min read",
+        intTime: 5,
+        intDate: new Date(2017, 9, 18),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/love.jpeg",
         content: "https://docs.google.com/document/d/e/2PACX-1vSps2EjlZ4tEkXWEbgqW8xsPKb8b09xMPfnU3fGpkPXDZlOCh_98wsc_kIAM6R8H-tppI0TY7TIwvly/pub?embedded=true"
       };
@@ -1399,6 +1409,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Oct 8, 2017",
         readTime: "8 min read",
+        intTime: 8,
+        intDate: new Date(2017, 9, 8),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/confusion.jpg"
       };
       var writing18 = {
@@ -1408,6 +1420,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "May 5, 2017",
         readTime: "20 min read",
+        intTime: 20,
+        intDate: new Date(2017, 4, 5),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/JuBu.jpg"
       };
       var writing17 = {
@@ -1417,6 +1431,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Dec 27, 2016",
         readTime: "2 min read",
+        intTime: 2,
+        intDate: new Date(2016, 11, 27),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Caleb+and+Ben.JPG",
         content: "https://docs.google.com/document/d/e/2PACX-1vRL4erU3mg5573byMRGttZyVp6SZ55FCvFVQIc7D4HtmCyXrMvU7xbfD6flb4RhWUf9bn_uoZ2r1ogS/pub?embedded=true"
       };
@@ -1427,6 +1443,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Dec 25, 2016",
         readTime: "2 min read",
+        intTime: 2,
+        intDate: new Date(2016, 11, 25),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/no-disconnect.png",
         content: "https://docs.google.com/document/d/e/2PACX-1vReE6zhkxn22XN0qFwI61MnPWQa2WRoDBVcWZxf0Wi9RleO0imcftMW1a9O44np2Hcj02ZzDUle6OTO/pub?embedded=true"
       };
@@ -1437,6 +1455,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Aug 15, 2015",
         readTime: "30 min read",
+        intTime: 30,
+        intDate: new Date(2015, 7, 15),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Mr-Nobody.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQoQCGzB2GhtU4KW3vtpKuv53IdYbCFFesmfsPm_OXnyenJSMU-SiO6n0duuQptLfdcHJA84M8q914h/pub?embedded=true"
       };
@@ -1447,6 +1467,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 30, 2015",
         readTime: "5 min read",
+        intTime: 5,
+        intDate: new Date(2015, 6, 30),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/A-Pale-View-Of-Hills.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vRo_qSD3ZSCocI-ajZJ-PF0y5HW5RaMXsm9sAhz6AWKRF6wIgQ4-jO26Y8X3CcwB0_unl48hLCSmCm6/pub?embedded=true"
       };
@@ -1457,6 +1479,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 26, 2018",
         readTime: "15 min read",
+        intTime: 15,
+        intDate: new Date(2015, 6, 26),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/anxiety.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vRDAmegsrrbhdNEd-yPXq-5ccUY8hXKl0-lsUgAARaGat0Mrqn1TaKMkDIIWUE11pwWGJ0kbeyz6Zlz/pub?embedded=true"
       };
@@ -1467,6 +1491,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 25, 2015",
         readTime: "15 min read",
+        intTime: 15,
+        intDate: new Date(2015, 6, 25),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/adventure.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQtNxrGyVfBlvH3pzIFUR0DwnIRZ5gS_Iw_S2RAgQbLuQJK60eOXCmlp_kq8NHAH6CtpiC8kCeAvgLS/pub?embedded=true"
       };
@@ -1477,6 +1503,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 24, 2015",
         readTime: "4 min read",
+        intTime: 4,
+        intDate: new Date(2015, 6, 24),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/marijuana4.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vSVnugR2TSUW0rUqvyZKj8JFMqxUDhXVSoK6xJShux3rFhFRDWB6fUP0Hkl2FJ5TcmtwmeBy_so_lqM/pub?embedded=true"
       };
@@ -1487,6 +1515,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 24, 2015",
         readTime: "3 min read",
+        intTime: 3,
+        intDate: new Date(2015, 6, 24),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/strength.png",
         content: "https://docs.google.com/document/d/e/2PACX-1vQoQCGzB2GhtU4KW3vtpKuv53IdYbCFFesmfsPm_OXnyenJSMU-SiO6n0duuQptLfdcHJA84M8q914h/pub?embedded=true"
       };
@@ -1497,6 +1527,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 23, 2015",
         readTime: "10 min read",
+        intTime: 10,
+        intDate: new Date(2015, 6, 23),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Mr-Nobody.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQoQCGzB2GhtU4KW3vtpKuv53IdYbCFFesmfsPm_OXnyenJSMU-SiO6n0duuQptLfdcHJA84M8q914h/pub?embedded=true"
       };
@@ -1507,6 +1539,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 22, 2015",
         readTime: "4 min read",
+        intTime: 4,
+        intDate: new Date(2015, 6, 22),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/distraction.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vTDOj3qzvQY4fGq3RvwEEpX-jgE7-72vgQ3J8DZDD7qDTrHGusITU8M7YVJbNxXMlRBryVxEUs7IfEz/pub?embedded=true"
       };
@@ -1517,6 +1551,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 20, 2015",
         readTime: "10 min read",
+        intTime: 10,
+        intDate: new Date(2015, 6, 20),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Mr-Nobody.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQoQCGzB2GhtU4KW3vtpKuv53IdYbCFFesmfsPm_OXnyenJSMU-SiO6n0duuQptLfdcHJA84M8q914h/pub?embedded=true"
       };
@@ -1527,6 +1563,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 13, 2015",
         readTime: "3 min read",
+        intTime: 3,
+        intDate: new Date(2015, 6, 13),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/No+Judgement+.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vRrNO7RiolqtlqIBMfYGETHwQCfGUzBxqZ4GeM5w9LOiE4g-FAErihn77uK8SCN6ptxLs6INKOPLo8Y/pub?embedded=true"
       };
@@ -1537,6 +1575,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 12, 2015",
         readTime: "10 min read",
+        intTime: 10,
+        intDate: new Date(2015, 6, 12),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Mr-Nobody.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQoQCGzB2GhtU4KW3vtpKuv53IdYbCFFesmfsPm_OXnyenJSMU-SiO6n0duuQptLfdcHJA84M8q914h/pub?embedded=true"
       };
@@ -1547,6 +1587,8 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 9, 2015",
         readTime: "6 min read",
+        intTime: 6,
+        intDate: new Date(2015, 6, 9),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Easter-Bunny-Food.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQQ6uN3L8LRn2UDJgg54eMXuLj7YdsLmqX4_MpWyA0ynhVgAEpsB9FQ43wlo13Ee71p28T7_milnkNz/pub?embedded=true"
       };
@@ -1557,16 +1599,20 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 8, 2015",
         readTime: "3 min read",
+        intTime: 3,
+        intDate: new Date(2015, 6, 8),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/Yellow_cat.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQ99jLIoMqDT3BsroJt0sxCeCpnfaZf_FjNcqjVAk5RwfB3jm0HPfaNUirdm3faPQSvN0zz1tvdhUoD/pub?embedded=true"
       };
       var writing2 = {
         id: 2,
         title: "Lunch",
-        blurb: "An essay written shortly after my father died.",
+        blurb: "An essay about lunch and sex.",
         author: "Benji Rothman",
         datePublished: "Jul 7, 2015",
         readTime: "5 min read",
+        intTime: 5,
+        intDate: new Date(2015, 6, 7),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/lunch.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vQpUpXWIRYZxMq1XiLj6l2ETB50j-hluD7cWO2QsAr2YUGOR6QXrWAkE1PLl4tUUBJGmbFW5QCFYNnn/pub?embedded=true"
       };
@@ -1577,11 +1623,47 @@ function (_React$Component) {
         author: "Benji Rothman",
         datePublished: "Jul 6, 2015",
         readTime: "8 min read",
+        intTime: 8,
+        intDate: new Date(2015, 6, 6),
         imageUrl: "https://s3.us-east-2.amazonaws.com/benji-personal-website/writing-images/summer-day.jpg",
         content: "https://docs.google.com/document/d/e/2PACX-1vToCKnJ7mMAbK7ZCcAT53xNtPIkP6HQQrs2nhbmjYilBmGKbqwkXBhfMPoQ-ZSacdlkPXjV33liUaje/pub?embedded=true"
       };
       var writingArr = [writing24, writing23, writing22, writing21, writing20, writing19, writing18, writing17, writing16, writing15, writing14, writing13, writing12, writing11, writing10, writing9, writing8, writing7, writing6, writing5, writing4, writing3, writing2, writing1];
-      var writingIndexItemArr = writingArr.map(function (writing) {
+      this.setState({
+        writingArr: writingArr
+      });
+    }
+  }, {
+    key: "filterByDate",
+    value: function filterByDate() {
+      var sortedArr = this.state.writingArr.sort(function (a, b) {
+        return a.intDate - b.intDate;
+      });
+      this.setState({
+        writingArr: sortedArr
+      });
+    }
+  }, {
+    key: "filterByTimeToRead",
+    value: function filterByTimeToRead() {
+      var sortedArr = this.state.writingArr.sort(function (a, b) {
+        return a.intTime - b.intTime;
+      });
+      this.setState({
+        writingArr: sortedArr
+      });
+    }
+  }, {
+    key: "viewWriting",
+    value: function viewWriting(id) {
+      this.props.history.push("/writings/".concat(id));
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var writingIndexItemArr = this.state.writingArr.map(function (writing) {
         return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_writing_index_item__WEBPACK_IMPORTED_MODULE_2__["default"], {
           key: writing.id,
           writing: writing,
@@ -1592,7 +1674,10 @@ function (_React$Component) {
         className: "writings-container"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
         className: "writing-header"
-      }, "My Writings"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, "My Writings"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        className: "sort-by-date",
+        onClick: this.filterByDate
+      }, "Sort By Date"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "writing-index-items-container"
       }, writingIndexItemArr));
     }
--- staticServer.js
+++ staticServer.js
@@ -1,8 +1,17 @@
 const connect = require('connect');
 const serveStatic = require('serve-static');
+const express = require('express');
+const app = express();
 
 const port = 3000;
 
 connect().use(serveStatic(__dirname)).listen(port, () => {
     console.log(`Server is now running on Port ${port}`);
 });
+
+//commented out because it only serves html for root response, i.e. '/'
+// app.get('/*', serveStatic(__dirname));
+
+// app.listen(port, () => {
+//     console.log(`Server is now running on Port ${port}`);
+// });
